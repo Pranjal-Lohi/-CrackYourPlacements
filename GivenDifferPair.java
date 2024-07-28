185ba2c4a50a66b3742b8fe7479393640ea5e51b
Pranjal-Lohi <pranjallohi345@gmail.com>
2024-07-28 15:32:32 +0530

    Day4#CrackYourPlacements

---
+++ GivenDifferPair.java
@@ -0,0 +1,10 @@
+public class GivenDifferPair {
+    public int findPair(int n, int x, int[] arr) {
+       HashSet<Integer> set=new HashSet<>();
+       for(int i=0;i<n;i++){
+           if(set.contains(arr[i]-x)||set.contains(arr[i]+x)) return 1;
+           set.add(arr[i]);
+       }
+       return -1;
+    }
+}
---
+++ ReverseWords.java
@@ -0,0 +1,10 @@
+public class ReverseWords {
+    public String reverseWords(String s) {
+        String[] str = s.trim().split("\\s+");
+        String out = "";
+        for(int i = str.length - 1;i>0;i--) {
+            out += str[i] + " ";
+        }
+        return out + str[0];
+    }
+}
