8312b9c9984403e6acd1998a20364c6813606503
Pranjal-Lohi <pranjallohi345@gmail.com>
2024-08-19 16:07:52 +0530

    Day26#CrackYourPlacements

---
+++ KthSmallestInMatrix.java
@@ -0,0 +1,16 @@
+import java.util.PriorityQueue;
+
+public class KthSmallestInMatrix {
+    public int kthSmallest(int[][] matrix, int k) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        for(int i = 0; i<matrix.length; i++)
+            for(int j=0;j<matrix[0].length;j++)
+                pq.add(matrix[i][j]);
+        
+       
+        while(k-- > 1)
+            pq.poll();
+        
+        return pq.poll();
+    }
+}
